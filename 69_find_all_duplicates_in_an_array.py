6f32548297fd758ff437219a5b4aa2681d4d98cd
Fiddle <fadil.nohur@outlook.com>
2025-05-17 20:46:40 +0100

    Create 69_find_all_duplicates_in_an_array.py

---
+++ 69_find_all_duplicates_in_an_array.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def solve(nums):
+  counter = Counter(nums)
+  dupes = []
+  for num, count in counter.items():
+    if count == 2:
+      dupes.append(num)
+  return sorted(dupes)
