15fda2960b230d6622911a91d127607f07979bf4
Fiddle <fadil.nohur@outlook.com>
2025-05-24 08:04:33 +0100

    Create 20_reverse_linked_list.py

---
+++ 20_reverse_linked_list.py
@@ -0,0 +1,17 @@
+# Singly Linked List Definition
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+def solve(head):
+  if head is None:
+    return head
+  new_head = None
+  curr = head
+  while curr is not None:
+    next_ = ListNode(curr.val)
+    next_.next = new_head
+    new_head = next_
+    curr = curr.next
+  return new_head
