b7d9da823415cdb51308730f88cfe90dcae0205a
Fiddle <fadil.nohur@outlook.com>
2025-06-01 10:19:18 +0100

    Create 01_fizzbuzz.py

---
+++ 01_fizzbuzz.py
@@ -0,0 +1,14 @@
+def solve(n):
+  result = []
+  for num in range(1, n + 1):
+    str_out = ""
+    if num % 3 == 0:
+      str_out += "Fizz"
+    if num % 5 == 0:
+      str_out += "Buzz"
+    result.append(
+      str_out
+      if str_out
+      else num
+    )
+  return result
