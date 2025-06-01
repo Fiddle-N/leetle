39412f8f532832e86e66633a51ba68d73cb8aa4b
Fiddle <fadil.nohur@outlook.com>
2025-06-01 09:23:09 +0100

    Create 08_two_sum.py

---
+++ 08_two_sum.py
@@ -0,0 +1,7 @@
+from itertools import combinations
+
+def solve(nums, target):
+  for x, y in combinations(enumerate(nums), 2):
+    if x[1] + y[1] == target:
+      return sorted([x[0], y[0]])
+  return []
