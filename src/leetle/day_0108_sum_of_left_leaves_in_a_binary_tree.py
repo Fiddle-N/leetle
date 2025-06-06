836a6e3ed4f7b3db30cddd77ff7780dde5bba7ce
Fiddle <fadil.nohur@outlook.com>
2025-06-06 20:17:34 +0100

    Create day_0108_sum_of_left_leaves_in_a_binary_tree.py

---
+++ src/leetle/day_0108_sum_of_left_leaves_in_a_binary_tree.py
@@ -0,0 +1,34 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class SumLeftLeaves:
+
+  def __init__(self):
+    self._lsum = 0
+
+  def _solve(self, node, side=None):
+    if node is None:
+      return True
+    no_lchild = self._solve(node.left, "left")
+    no_rchild = self._solve(node.right, "right")
+    is_leaf = no_lchild and no_rchild
+    if (
+      side == "left"
+      and is_leaf
+    ):
+      self._lsum += node.val
+    return False
+
+  def solve(self, node):
+    self._solve(node)
+    return self._lsum
+    
+    
+def solve(root):
+  if root is None:
+    return 0
+  return SumLeftLeaves().solve(root)
