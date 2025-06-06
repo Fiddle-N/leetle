69a1e3fde0a64e5c946bf07eb4c0cd528f0db706
Fiddle <fadil.nohur@outlook.com>
2025-06-06 18:21:41 +0100

    Create day_0103_find_peak_element.py

---
+++ src/leetle/day_0103_find_peak_element.py
@@ -0,0 +1,22 @@
+def solve(nums):
+  if not nums:
+    return None
+  if len(nums) == 1:
+    return 0
+    
+  # length > 1
+  # always assume unambiguous solution
+  if nums[0] > nums[1]:
+    # check first element
+    return 0
+  for x in range(len(nums) - 3 + 1):
+    # check middle elements
+    seq = nums[x : x + 3]
+    if (
+      seq[1] > seq[0]
+      and seq[1] > seq[2]
+    ):
+      return x + 1
+  if nums[-1] > nums[-2]:
+    # check last element
+    return len(nums) - 1
