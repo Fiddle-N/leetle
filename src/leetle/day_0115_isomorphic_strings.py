6a45aadedd3c8915f2ab1a8da868791349737485
Fiddle <fadil.nohur@outlook.com>
2025-06-07 11:35:26 +0100

    Create day_0115_isomorphic_strings.py

---
+++ src/leetle/day_0115_isomorphic_strings.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+def solve(s, t):
+  # assume input is all lowercase ascii letters
+  # also this solution broke ChatGPT
+  return(
+    sorted(Counter(s).values())
+    == sorted(Counter(t).values())
+  )
