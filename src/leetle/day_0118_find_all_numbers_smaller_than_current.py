bbde68410f0c4d9f9d239febcbe84e3b189d915e
Fiddle <fadil.nohur@outlook.com>
2025-06-07 14:33:13 +0100

    Create day_0118_find_all_numbers_smaller_than_current.py

---
+++ src/leetle/day_0118_find_all_numbers_smaller_than_current.py
@@ -0,0 +1,17 @@
+def solve(nums):
+  cum_counts = {}
+  prev = 0
+  prev_num = None
+  for num in sorted(nums):
+    if (
+      prev_num is None 
+      or num > prev_num
+    ):
+      cum_counts[num] = prev
+      prev_num = num
+    prev += 1
+
+  return [
+    cum_counts[num]
+    for num in nums
+  ]
