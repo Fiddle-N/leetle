9cce9e15a3b24d1fc55cbf8223ffbaab862b6f91
Fiddle <fadil.nohur@outlook.com>
2025-06-14 09:47:06 +0100

    Create day_0140_digits_product.py

---
+++ src/leetle/day_0140_digits_product.py
@@ -0,0 +1,42 @@
+from collections import deque
+from dataclasses import dataclass
+
+@dataclass
+class Item:
+  product: int
+  digits: list[int]
+
+def solve(product):
+  if product == 0:
+    return 10
+  if product == 1:
+    return 1
+  q = deque([
+    Item(
+      product,
+      digits=[]
+    )
+  ])
+  while True:
+    if not q:
+      return -1
+    item = q.popleft()
+    for div in range(2, 10):
+      quot, mod = divmod(item.product, div)
+      if mod != 0:
+        continue
+      digits = item.digits.copy()
+      digits.append(div)
+      if quot == 1:
+        return int(
+          ''.join(
+            str(digit) 
+            for digit in digits
+          )
+        )
+      q.append(
+        Item(
+          quot,
+          digits
+        )
+      )
